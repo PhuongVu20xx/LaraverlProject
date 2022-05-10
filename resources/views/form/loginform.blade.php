95b99f13c3a311d9b04dfe274932cafdd2189cbc
Lip Daniel <89788727+LipDaniel@users.noreply.github.com>
2022-05-10 21:07:10 +0700

    update form

---
+++ resources/views/form/loginform.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout')
+<!-- HEADER -->
+@section('header')
+    @include('layouts.header')
+@endsection
+
+<!-- LOGIN-FORM -->
+@section('body')
+    
+@endsection
+<!-- FOOTER -->
+@section('footer')
+    @include('layouts.footer')
+@endsection
