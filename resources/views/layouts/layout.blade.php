7313f310d69bdcf50b7723a63034b765e241f689
phongvan-1412 <85114119+phongvan-1412@users.noreply.github.com>
2022-05-10 23:00:44 +0700

    update css: footer, php: footer+layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @yield('css')
-    <link rel="stylesheet" href="{{asset('css\bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
 
 </head>
