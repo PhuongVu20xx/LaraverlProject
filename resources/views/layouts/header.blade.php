c84423b20607baba626deea36dcaa0f5a46e020e
phongvan-1412 <85114119+phongvan-1412@users.noreply.github.com>
2022-05-10 19:39:59 +0700

    create img folder+update landingpage,footer,header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,16 +1,25 @@
+<link rel="stylesheet" href="{{asset('css/header.css')}}">
 <link href="https://fonts.googleapis.com/css2?family=Babylonica&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/8b058784b8.js" crossorigin="anonymous"></script>
 <div class="header">
     <div class="navigation">
-        <div><h4 class="homebtn"><a href="header/home.php"><i class="fa-solid fa-house"></i> Home</a></h4></div>
+        <div class="homebtn"><h4 ><a href="header/home.php"><i class="fa-solid fa-house"></i> Home</a></h4></div>
         <div class="nav-container">
-            <a href="header/needhelp.php">Need Help?</a>
+            <a href="#">About</a>
+            <a href="#">Cart</a>
+            <a href="#">Shipping</a>
             <a href="header/signup.php">Sign Up |</a>
             <a href="header/login.php">Login |</a>
             <a href="#"><i class="fa-solid fa-earth-asia"></i></a>
-        </div>    
-        <div class="logo">
-            <img src="#" alt="logo">
-        </div>   
+        </div>       
+    </div>
+    <div class="thumbnail">
+        <div class="content">
+            <h1>VNPH'S GROCERY STORE</h1>
+            <p>We care about your family, come and join with us.</p>
+            <button><b>Shopping</b></button>
+            <button><b>Feedback</b></button>
+        </div>
+        <img src="{{asset('img/thumbnail.jpg')}}" alt="logo">
     </div>
 </div>
