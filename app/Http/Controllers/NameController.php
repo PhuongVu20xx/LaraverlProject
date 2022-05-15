a1ac8a59ca67da02782370eac9b6f0c3a4ab2238
PhuongVu20xx <103616657+PhuongVu20xx@users.noreply.github.com>
2022-05-15 16:52:40 +0700

    Update name controller

--- app/Http/Controllers/NameController.php
+++ app/Http/Controllers/NameController.php
@@ -1,16 +1,71 @@
 <?php
-    const $ADMIN = "admin";
-    const $INFOMATION = "infomation";
-    const $NAVIGATOR = "navigator";
-    const $EDITPRODUCT = "editproduct";
-    const $ADDPRODUCT = "addproduct";
-    const $ADDCATEGORY = "addcategory";
-    const $CONTROLLER = "controller";
-    const $CONTROLLERS = "controllers";
-    const $CUSTOMMER = "custommer";
-    const $FEEDBACK = "feedback";
-    const $LASTESTORDER = "lastestorder";
-    const $LAYOUTS = "layouts";
-    const $LAYOUT = "layout";
 
-?>
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NameController
+{
+ // A
+ static $ADD_CATEGORY = "addcategory";
+ static $ADD_PRODUCT = "addproduct";
+ static $ADMIN = "admin";
+ //B
+ //C
+ static $CONTROLLER = "controller";
+ static $CONTROLLERS = "controllers";
+ static $CUSTOMMER = "custommer";
+ //D
+ //E
+ static $EDIT_PRODUCT = "editproduct";
+
+ //F
+ static $FEEDBACK = "feedback";
+ //G
+ //H
+ //I
+ static $INFOMATION = "infomation";
+ static $IMPORT_DAY = "importday";
+ static $IMPORT_PROCUCT = "importproduct";
+ //J
+ //K
+ //L
+ static $LAYOUT = "layout";
+ static $LAYOUTS = "layouts";
+ static $LASTEST_ORDER = "lastestorder";
+ //M
+ //N
+ static $NAME = "name";
+ static $NAVIGATOR = "navigator";
+ //O
+ //P
+ static $PRICE = "price";
+ static $PRODUCER = "producer";
+ static $PRODUCT = "product";
+ //Q
+ static $QUANTITY = "quantity";
+ //R
+ //S
+ //T
+ //U
+ //V
+ //W
+ //X
+ //Y
+ //Z
+
+ //Arlet
+ static $IMPORT_FAIL = "Import Fail";
+ static $IMPORT_SUCCESS = "Import Success!";
+
+ //Views
+ static $ADMIN_CONTROLLER = "admin.controller";
+ static $ADMIN_CONTROLLERS_ADD_CATEGORY = "admin.controllers.addcategory";
+ static $ADMIN_CONTROLLERS_ADD_PRODUCT = "admin.controllers.addproduct";
+ static $ADMIN_CONTROLLERS_ALL_PRODUCT = "admin.controllers.allproduct";
+ static $ADMIN_CONTROLLERS_EDIT_PRODUCT = "admin.controllers.editproduct";
+ static $ADMIN_CONTROLLERS_IMPORT_PRODUCT = "admin.controllers.importproduct";
+ static $ADMIN_CUSTOMER = "admin.customer";
+ static $ADMIN_FEEDBACK = "admin.feedback";
+ static $ADMIN_LATEST_ORDER = "admin.lastestorder";
+}
