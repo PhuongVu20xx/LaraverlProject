6a9931ef26faa053f8f9c204257928705f6d1495
PhuongVu20xx <103616657+PhuongVu20xx@users.noreply.github.com>
2022-05-15 10:37:06 +0700

    Create const string file

---
+++ app/Http/Controllers/NameController.php
@@ -0,0 +1,16 @@
+<?php
+    const $ADMIN = "admin";
+    const $INFOMATION = "infomation";
+    const $NAVIGATOR = "navigator";
+    const $EDITPRODUCT = "editproduct";
+    const $ADDPRODUCT = "addproduct";
+    const $ADDCATEGORY = "addcategory";
+    const $CONTROLLER = "controller";
+    const $CONTROLLERS = "controllers";
+    const $CUSTOMMER = "custommer";
+    const $FEEDBACK = "feedback";
+    const $LASTESTORDER = "lastestorder";
+    const $LAYOUTS = "layouts";
+    const $LAYOUT = "layout";
+
+?>
