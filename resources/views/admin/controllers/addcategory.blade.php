4f4c66c5ac07e3003b647f5f0c9954fb32f89dff
PhuongVu20xx <103616657+PhuongVu20xx@users.noreply.github.com>
2022-05-15 10:41:03 +0700

    Create add category view

---
+++ resources/views/admin/controllers/addcategory.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.layout')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/adminnavigatortab.css') }}">
+@endsection
+
+@section('header')
+    @include('layouts.header')
+@endsection
+
+@section('menu')
+    @include('admin.navigator')
+@endsection
+
+@section('bodyheader')
+    <div>
+        <h3>Add Category</h3>
+        <hr>
+    </div>
+@endsection
+
+@section('bodynav')
+    @include('admin.controllers.navigator')
+@endsection
+
+@section('bodycontent')
+    <div>
+        <form action="/addcategory" method="post">
+            <div>
+                Name category: <input type="text" id="name" class="name">
+                <input type="submit" id="submit" class="submit" value="Submit">
+            </div>
+        </form>
+    </div>
+@endsection
+
+@section('footer')
+    @include('layouts.footer')
+@endsection
