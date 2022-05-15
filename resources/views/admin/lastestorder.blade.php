516bb1112483c218d78175a733e23a7dd936a9e7
PhuongVu20xx <103616657+PhuongVu20xx@users.noreply.github.com>
2022-05-15 10:39:57 +0700

    Update lastestorder view

--- resources/views/admin/lastestorder.blade.php
+++ resources/views/admin/lastestorder.blade.php
@@ -2,72 +2,65 @@
 
 @section('css')
 
-<head>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/respose-tableadminfeedback.css')}}">
-</head>
-
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/respose-tableadminfeedback.css') }}">
+    </head>
 @endsection
 
 @section('header')
-@include('layouts.header')
+    @include('layouts.header')
 @endsection
 
 @section('menu')
-@include('admin.navigator')
+    @include('admin.navigator')
 @endsection
 
-@section('content')
-
-<body>
+@section('bodyheader')
     <div>
         <h3>Latest Order</h3>
+        <hr>
     </div>
+@endsection
 
+@section('bodycontent')
     <section class="bg-white p-5">
         <div id="no-more-tables" class="content">
             <div class="clearfix"> </div>
             <div class="clearfix"></div>
+            <div class="table-responsive ">
+                <table id="myTable" class="table bg-white">
+                    <thead class="bg-dark">
+                        <tr>
+                            <th class="text-light">Content</th>
+                            <th class="text-light">Name</th>
+                            <th class="text-light">Time</th>
+                        </tr>
+                    </thead>
 
-            <div class="box box-primary">
-                <div class="box-body">
-                    <div class="table-responsive ">
-                        <table id="myTable" class="table bg-white">
-                            <thead class="bg-dark">
-                                <tr>
-                                    <th class="text-light">Content</th>
-                                    <th class="text-light">Name</th>
-                                    <th class="text-light">Time</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                @for ($i = 0; $i <= 100; $i++)<tr>
-                                    <td data-title="Content">Content {{$i}} </td>
-                                    <td data-title="Name">Name {{$i}}</td>
-                                    <td data-title="Time">Time {{$i}}</td>
-                                    </tr>
-                                    @endfor
-                            </tbody>
-                    </div>
-                    </table>
-                </div>
+                    <tbody>
+                        @for ($i = 0; $i <= 100; $i++)
+                            <tr>
+                                <td data-title="Content">Content {{ $i }} </td>
+                                <td data-title="Name">Name {{ $i }}</td>
+                                <td data-title="Time">Time {{ $i }}</td>
+                            </tr>
+                        @endfor
+                    </tbody>
+                </table>
             </div>
-
         </div>
     </section>
+@endsection
 
-    @endsection
-
-    @section('footer')
+@section('footer')
     @include('layouts.footer')
-    @endsection
+@endsection
 
-    @section('scripts')
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js">
-    </script>
+@section('scripts')
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="//cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
-    <script src="{{asset('js/tableadminfeedback.js')}}"></script>
-    @endsection
+    <script src="{{ asset('js/tableadminfeedback.js') }}"></script>
+@endsection
