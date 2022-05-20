a93ffbd7e20b06f6c86cb90f63a32c1b8020a647
HANHTRAN2710 <103866643+HANHTRAN2710@users.noreply.github.com>
2022-05-20 23:28:05 +0700

    updateblog

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -16,13 +16,25 @@
          <div class="desc">Blog</div>
     </div>          
     <!--  -->
+    
     <div class="row">
+        <div class="headerblog">
+            <h4><a href="#">Home</a></h4>
+            <p>/</p>
+            <p>blog</p>
+        </div>
         <!-- left -->
         <div class="col-md-8 main">
             <div class="blog-items">
-                <div><img src="{{asset('img/landingpage5.jpg')}}" alt="Garlic"></div>
+                <div>
+                    <img src="{{asset('img/landingpage5.jpg')}}" alt="Garlic">
+                </div>
+                <!-- <div>
+                         <p>28</p>
+                         <p>Dec,2022</p>
+                </div> -->
                 <div class="blog-item">
-                    <h4>COOKING RECIPE DELICIOUS</h4>
+                    <h4><a href="#"> COOKING RECIPE DELICIOUS</a></h4>
                     <ul>
                         <li>by Admin |</li>
                         <li>28 December,2022 |</li>
@@ -38,7 +50,7 @@
             <div class="blog-items">
                 <div><img src="{{asset('img/landingpage5.jpg')}}" alt="Garlic"></div>
                 <div class="blog-item">
-                    <h4>PIZZA IS PREPARED FRESH</h4>
+                    <h4><a href="#">PIZZA IS PREPARED FRESH</a> </h4>
                     <ul>
                         <li>by Admin |</li>
                         <li>28 December,2022 |</li>
@@ -53,7 +65,7 @@
             <div class="blog-items">
                 <div><img src="{{asset('img/landingpage5.jpg')}}" alt="Garlic"></div>
                 <div class="blog-item">
-                    <h4>STYLE THE WEDDING PARTY</h4>
+                    <h4><a href="#">STYLE THE WEDDING PARTY</a> </h4>
                     <ul>
                         <li>by Admin |</li>
                         <li>28 December,2022 |</li>
@@ -68,7 +80,7 @@
             <div class="blog-items">
                 <div><img src="{{asset('img/landingpage5.jpg')}}" alt="Garlic"></div>
                 <div class="blog-item">
-                    <h4>BEST PLACES FOR WINE</h4>
+                    <h4><a href="#"> BEST PLACES FOR WINE</a></h4>
                     <ul>
                         <li>by Admin |</li>
                         <li>28 December,2022 |</li>
@@ -83,14 +95,14 @@
 
         </div>
         <!-- right -->
-        <div class="col-md-4">
+        <div class="col-md-4 main2">
 
             <div class="search">
                 <input type="text" name="search" placeholder="Search"  >
                 <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
             </div>
             <div class="categori">
-                <h4>categories</h4>
+                <h4><a href="#">categories</a> </h4>
                 <ul >
                     <li><a href="#">COOKING RECIPE</a> </li>
                     <li><a href="">DELICIOUS FOODS</a> </li>
@@ -101,7 +113,7 @@
             </div>
             <!-- popular -->
             <div class="popular">
-                <h4>MOST POPULAR</h4>
+                <h4><a href="#">MOST POPULAR</a> </h4>
                 <ul>
                     <li>
                         <div class="imh">
@@ -152,16 +164,41 @@
             </div>
             <!-- Archive -->
             <div class="archive">
-                <h4>Archive</h4>
+                <h4><a href="#">Archive</a> </h4>
                 <ul>
-                    <li><a href="#">uly 2022</a></li>
-                    <li><a href="#">june 2022</a></li>
-                    <li><a href="#">may 2022</a></li>
-                    <li><a href="#">april 2022</a></li>
-                    <li><a href="#">march 2022</a></li>
-                    <li><a href="#">february 2022</a></li>
-                    <li><a href="#">january</a></li>
-                    <li><a href="#">december 2021</a></li>
+                    <li>
+                        <a href="#">uly 2022  </a>
+                        <span>(6)</span>
+                    </li>
+                    <li>
+                        <a href="#">june 2022</a>
+                        <span>(39)</span>
+                    </li>
+                    <li>
+                        <a href="#">may 2022</a>
+                        <span>(25)</span>
+                    </li>
+                    <li>
+                        <a href="#">april 2022</a>
+                        <span>(27)</span>
+                    </li>
+                    <li>
+                        <a href="#">march 2022</a>
+                        <span>(29)</span>
+                    </li>
+                    <li>
+                        <a href="#">february 2022</a>
+                        <span>(28)</span>
+                    </li>
+                    <li>
+                        <a href="#">january</a>
+                        <span>(24)</span>
+                    </li>
+                        
+                    <li>
+                        <a href="#">december 2021</a>
+                        <span>(23)</span>
+                    </li>
                 </ul>
             </div>
         </div>
