6a75198f7624b2b1dbb1b524c4defcb61ceb5387
phongvan-1412 <85114119+phongvan-1412@users.noreply.github.com>
2022-05-21 00:35:16 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -47,9 +47,11 @@ Route::get('/blog',function(){
 
 
 Route::get('/product',function(){
-    return view('pages.product');
+    return view('products/product');
 });
-Route::post('/product', [SearchController::class,'index']);
+Route::get('/product/sweet-grocery',function(){
+    return view('products/sweetgrocery');
+});
 
 
 // Login
