347fde1372fd2600c20bcfa534993c162590bc74
Lip Daniel <89788727+LipDaniel@users.noreply.github.com>
2022-06-02 13:10:13 +0700

    update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,24 +11,24 @@ class ProfileController extends Controller
     }
 
     public function postProfileForm(Request $request){
-        
+        $email = $request->email;
+        $avatar = $request->file('avatar');
+        $avatarPath = $avatar->move('upload', $avatar->getClientOriginalName());
+
+        // DB::table('user')->where('email',$email)->update(['image_name' => $avatarPath]);
     }
 
     public function postNewPassword(Request $request){
         $hiddenemail = $request->hiddenemail;
         $currentpwd = md5($request->currentpwd);
         $newpwd = md5($request->newpwd);
-        $confirmpwd = md5($request->confirmpwd);
 
-        if($newpwd == $confirmpwd){
-            // DB::select( "call sp_change_password_by_email($hiddenemail,$newpwd,$currentpwd)");
-            DB::table('user')->where('email', $hiddenemail)
-                            ->where('password', $currentpwd)
-                            ->update(['password' => $newpwd]);  
-            return redirect()->back()->withInput()->with('thongbao','Changed Password Successfully!');
-        }else{
-            return redirect()->back()->withInput()->with('alert','Wrong confirm password');
-        }
+        // DB::select( "call sp_change_password_by_email($hiddenemail,$newpwd,$currentpwd)");
+        DB::table('user')->where('email', $hiddenemail)
+                        ->where('password', $currentpwd)
+                        ->update(['password' => $newpwd]);  
+        return redirect()->back()->withInput()->with('thongbao','Changed Password Successfully!');
+
     }      
 }
 
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -11,128 +11,126 @@
 @endsection
 <!-- BODY -->
 @section('body')
-<div class="container-xl px-4 mt-4">
-    <hr class="mt-0 mb-4">
-    <div class="row">
-        <div class="col-xl-4">
-            <!-- Profile picture card-->
-            <div class="card mb-4 mb-xl-0">
-                <div class="card-header">Profile Picture</div>
-                <div class="card-body text-center">
-                    <!-- Profile picture image-->
-                    <img class="img-account-profile rounded-circle mb-2" src="{{asset('upload/default-profile.jpg')}}" alt="">
-                    <!-- Profile picture help block-->
-                    <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
-                    <!-- Profile picture upload button-->
-                    <input type="file">
+<div>
+    <div class="container-xl px-4 mt-4">
+        <hr class="mt-0 mb-4">
+        <div class="row">
+            <div class="col-xl-4">
+                <form action="profile" method="POST" enctype="multipart/form-data">
+                    {{csrf_field()}}
+                <!-- Profile picture card-->
+                <div class="card mb-4 mb-xl-0">
+                    <div class="card-header">Profile Picture</div>
+                    <div class="card-body text-center">
+                        <!-- Profile picture image-->
+                        <img class="img-account-profile rounded-circle mb-2" src="{{asset('upload/default-profile.jpg')}}" alt="">
+                        <!-- Profile picture help block-->
+                        <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                        <!-- Profile picture upload button-->
+                        <input type="file" name="avatar">
+                    </div>
+                </div>
+                <div class="card mt-2">
+                    <span class="btn btn-primary" id="changepwd">Change Password</span>
                 </div>
             </div>
-            <div class="card mt-2">
-                <span class="btn btn-primary" id="changepwd">Change Password</span>
-            </div>
-        </div>
 
- 
-        <!-- ACCOUNT DETAIL -->
-        <div class="col-xl-8" id="account-detail">
-            <!-- Account details card-->
-            <div class="card mb-4">
-                <div class="card-header">Account Details</div>
-                <div class="card-body">
-                    <form action="profile" method="POST" enctype="multipart/form-data">
-                        {{csrf_field()}}
-                        <!-- Form Group (username)-->
-                        <div class="mb-3">
-                            <label class="small mb-1" for="inputUsername">Rank</label>
-                            <input class="form-control" id="inputUsername" type="text" value="Platinum" disabled>
-                        </div>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (first name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputFirstName">First name</label>
-                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie" disabled>
+            <!-- ACCOUNT DETAIL -->
+            <div class="col-xl-8" id="account-detail">
+                <!-- Account details card-->
+                <div class="card mb-4">
+                    <div class="card-header">Account Details</div>
+                    <div class="card-body">
+
+                            <!-- Form Group (username)-->
+                            <div class="mb-3">
+                                <label class="small mb-1" for="inputUsername">Rank</label>
+                                <input class="form-control" id="inputUsername" type="text" value="Platinum" disabled>
                             </div>
-                            <!-- Form Group (last name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputLastName">Last name</label>
-                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna" disabled>
+                            <!-- Form Row-->
+                            <div class="row gx-3 mb-3">
+                                <!-- Form Group (first name)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputFirstName">First name</label>
+                                    <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie" disabled>
+                                </div>
+                                <!-- Form Group (last name)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputLastName">Last name</label>
+                                    <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <!-- Form Row        -->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (organization name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputOrgName">Organization name</label>
-                                <input class="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="Start Bootstrap">
+                            <!-- Form Row-->
+                            <div class="row gx-3 mb-3">
+                                <!-- Form Group (organization name)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputOrgName">Organization name</label>
+                                    <input class="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="Start Bootstrap">
+                                </div>
+                                <!-- Form Group (location)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputLocation">Location</label>
+                                    <input class="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="San Francisco, CA">
+                                </div>
                             </div>
-                            <!-- Form Group (location)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputLocation">Location</label>
-                                <input class="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="San Francisco, CA">
+                            <!-- Form Group (email address)-->
+                            <div class="mb-3">
+                                <label class="small mb-1" for="Email">Email address</label>
+                                <input class="form-control" id="Email" name="Email" type="email" placeholder="Enter your email address" value="xuanphuong@gmail.com" disabled>
                             </div>
-                        </div>
-                        <!-- Form Group (email address)-->
-                        <div class="mb-3">
-                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value="xuanphuong@gmail.com" disabled>
-                        </div>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (phone number)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputPhone">Phone number</label>
-                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567">
+                            <!-- Form Row-->
+                            <div class="row gx-3 mb-3">
+                                <!-- Form Group (phone number)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputPhone">Phone number</label>
+                                    <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567">
+                                </div>
+                                <!-- Form Group (birthday)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputBirthday">Birthday</label>
+                                    <input class="form-control" id="inputBirthday" type="date" name="birthday" placeholder="Enter your birthday" value="06/10/1988">
+                                </div>
                             </div>
-                            <!-- Form Group (birthday)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputBirthday">Birthday</label>
-                                <input class="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Enter your birthday" value="06/10/1988">
-                            </div>
-                        </div>
-                        <!-- Save changes button-->
-                        <button class="btn btn-primary" type="submit">Save changes</button>
-                    </form>
+                            <!-- Save changes button-->
+                            <button class="btn btn-primary" type="submit">Save changes</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- CHANGE PASSWORD -->
-        <div class="col-xl-8" id="changepwdform" style="display:none">
-            <div class="card mb-4">
-                <div class="card-header">Change Password <span class="btn" id="close" style="float: right;">x</span></div>
-                <div class="card-body">
-                    @if(session('thongbao'))
-                        <div class="alert alert-success">
-                            {{session('thongbao')}}
-                        </div>
-                    @endif
-                    <form action="/changepwd" method="POST">
-                        {{csrf_field()}}
-                        <div class="mb-3" hidden>
-                            <label class="small mb-1" for="hiddenemail">Email</label>
-                            <input class="form-control" id="hiddenemail" name="hiddenemail" type="email" value="xuanphuong@gmail.com">
-                        </div>
-                        <div class="mb-3">
-                            <label class="small mb-1" for="currentpwd">Current Password</label>
-                            <input class="form-control" id="currentpwd" name="currentpwd" type="password">
-                        </div>
-                        <div class="mb-3">
-                            <label class="small mb-1" for="newpwd">New Password</label>
-                            <input class="form-control" id="newpwd" name="newpwd" type="password">
-                        </div>
-                        <div class=" mb-3">
-                            <label class="small mb-1" for="confirmnewpwd">Confirm New Password</label>
-                            <input class="form-control" id="confirmpwd" name="confirmpwd" onkeyup="checkpwd()" type="password">
-                        </div>
-                        @if(session('alert'))
-                            <div class="mb-3 alert alert-danger" id="alert">
-                                {{session('alert')}}
+            <!-- CHANGE PASSWORD -->
+            <div class="col-xl-8" id="changepwdform" style="display:none">
+                <div class="card mb-4">
+                    <div class="card-header">Change Password <span class="btn" id="close" style="float: right;">Back</span></div>
+                    <div class="card-body">
+                        @if(session('thongbao'))
+                            <div class="alert alert-success">
+                                {{session('thongbao')}}
                             </div>
                         @endif
-                        <div>
-                            <button class="btn btn-primary" type="submit">Change Password</button>
-                        </div>
-                    </form>
+                        <form action="/changepwd" method="POST">
+                            {{csrf_field()}}
+                            <div class="mb-3" hidden>
+                                <label class="small mb-1" for="hiddenemail">Email</label>
+                                <input class="form-control" id="hiddenemail" name="hiddenemail" type="email" value="xuanphuong@gmail.com">
+                            </div>
+                            <div class="mb-3">
+                                <label class="small mb-1" for="currentpwd">Current Password</label>
+                                <input class="form-control" id="currentpwd" name="currentpwd" type="password">
+                            </div>
+                            <div class="mb-3">
+                                <label class="small mb-1" for="newpwd">New Password</label>
+                                <input class="form-control" id="newpwd" name="newpwd" type="password">
+                            </div>
+                            <div class=" mb-3">
+                                <label class="small mb-1" for="confirmpwd">Confirm New Password</label>
+                                <input class="form-control" id="confirmpwd" name="confirmpwd" onkeyup="checkpwd();" type="password">
+                                <div id="alert"></div>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary" id="changepwd-btn" type="submit" disabled>Change Password</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
