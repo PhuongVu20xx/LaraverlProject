4b4bb132eb3756197c3cc3bcec2cc4eebe94f68a
phuongvu <phuongvx@greenbeli.io>
2022-06-04 14:23:00 +0700

    Create all stock view

---
+++ resources/views/form/allStock.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.adminlayout')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/admin/adminnavigatortab.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/form/allproduct.css') }}">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/form/respose-tableadminfeedback.css') }}">
+@endsection
+
+@section('header')
+    @include('layouts.header')
+    <div class="backgroundheader"></div>
+@endsection
+
+@section('menu')
+    @include('admin.menu')
+@endsection
+
+@section('bodyheader')
+    <div>
+        <h3>Controller / Product / All Stock</h3>
+        <hr>
+    </div>
+@endsection
+
+@section('bodynav')
+    @include('form.formNavigator')
+@endsection
+
+@section('bodycontent')
+    <section class="bg-white p-5">
+        <div id="no-more-tables" class="content">
+            <div class="clearfix"> </div>
+            <div class="clearfix"></div>
+
+            <div class="table-responsive ">
+                <table id="myTable" class="table bg-white">
+                    <thead class="bg-dark">
+                        <tr>
+                            <th class="text-light">ID</th>
+                            <th class="text-light">Category Name</th>
+                            <th class="text-light">Product Name</th>
+                            <th class="text-light">Information</th>
+                            <th class="text-light">Image</th>
+                        </tr>
+                    </thead>
+
+                    {{-- <tbody>
+                        @if (count($allproduct) > 0)
+                            <span hidden> {{ $i = 1 }}</span>
+                            @foreach ($allproduct as $product)
+                                <tr>
+                                    <td>{{ $i++ }} </td>
+                                    <td>{{ $product->category_id }}</td>
+                                    <td>{{ $product->product_name }}</td>
+                                    <td>{{ $product->information }}</td>
+                                    <td><img src="{{ asset('upload.product') }}/{{ $product->img_name }}" id="img_product"
+                                            alt=""></td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody> --}}
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('footer')
+    @include('layouts.footer')
+@endsection
+
+@section('scripts')
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="//cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('js/tableadminfeedback.js') }}"></script>
+@endsection
