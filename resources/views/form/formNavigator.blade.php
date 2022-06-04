123cdbd1d91392988a62bdb84d19116114bd14ce
phuongvu <phuongvx@greenbeli.io>
2022-06-04 14:38:27 +0700

    Update admin form navigator

--- resources/views/form/formNavigator.blade.php
+++ resources/views/form/formNavigator.blade.php
@@ -2,8 +2,12 @@
     <ul class="nav nav-tabs">
         <li><a class="text-decoration-none" href="/addproduct" data-toggle="tab">Add Product</a></li>
         <li><a class="text-decoration-none" href="/allproduct" data-toggle="tab">All Product</a></li>
-        <li><a class="text-decoration-none" href="/editproduct" data-toggle="tab">Edit Product</a></li><br>
-        <li><a class="text-decoration-none" href="/importstock" data-toggle="tab">Import Stock</a></li><br>
-        <li><a class="text-decoration-none" href="/importproduct" data-toggle="tab">Import Product</a></li>
+        <li><a class="text-decoration-none" href="/editproduct" data-toggle="tab">Edit Product</a></li>
+        <li><a class="text-decoration-none" href="/importstock" data-toggle="tab">Import Stock</a></li>
+        <li><a class="text-decoration-none" href="/allstock" data-toggle="tab">All Stock</a></li>
+        <li><a class="text-decoration-none" href="/editstock" data-toggle="tab">Edit Stock</a></li>
+        <li><a class="text-decoration-none" href="/importproduct" data-toggle="tab">Add Import Product</a></li>
+        <li><a class="text-decoration-none" href="/allimportproduct" data-toggle="tab">All Import Product</a></li>
+        <li><a class="text-decoration-none" href="/editimportproduct" data-toggle="tab">Edit Import Product</a></li>
     </ul>
 </div>
