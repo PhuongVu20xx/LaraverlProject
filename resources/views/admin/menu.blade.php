32978a5804db8e9407844dc8502d0d36b211ae82
phuongvu <phuongvx@greenbeli.io>
2022-06-08 13:24:26 +0700

    Add ajax link

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,10 +1,9 @@
-
 <div class="m-3 ps-5  border border-danger border-start-0 border-top-0 border-bottom-0">
     <div class="m-1">
         <a class="text-decoration-none" href="/home">Home</a>
     </div>
     <div class="m-1">
-        <a class="text-decoration-none" href="/addcategory">Category</a>
+        <a class="text-decoration-none" id="category" href="#">Category</a>
     </div>
     <div class="m-1">
         <a class="text-decoration-none" href="/addproduct">Product</a>
@@ -14,7 +13,7 @@
     </div>
     <div class="m-1">
         <a class="text-decoration-none" href="/adminpayment">Payment</a>
-    </div>  
+    </div>
     <div class="m-1">
         <a class="text-decoration-none" href="/customer">Customer</a>
     </div>
@@ -25,3 +24,31 @@
         <a class="text-decoration-none" href="/lastestorder">Lastest Order</a>
     </div>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $('#category').click(function() {
+            $.ajax({
+                type: "GET",
+                url: "/addcategory",
+                success: function() {
+                    window.location.href = '/addcategory';
+                    //window.open("/addcategory", "_self");
+                    //$(location).attr('href', '/addcategory');
+                    //alert("Success");
+                }
+            });
+            // $.get("/addcategory", function() {
+            //     window.open("/addcategory", "_self");
+            //     alert("Success");
+            // });
+            //$("bodycontent").load("/addcategory");
+        });
+    });
+</script>
