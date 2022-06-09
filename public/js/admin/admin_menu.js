370d3b4901eef4c1a5d7ea093b68880b0ca007d7
phuongvu <phuongvx@greenbeli.io>
2022-06-09 19:12:39 +0700

    Add admin menu js

---
+++ public/js/admin/admin_menu.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $('#category').click(function() {
+        $.ajax({
+            type: "GET",
+            url: "/addcategory",
+            success: function() {
+                window.location.href = '/addcategory';
+                //window.open("/addcategory", "_self");
+                //$(location).attr('href', '/addcategory');
+                //alert("Success");
+            }
+        });
+        // $.get("/addcategory", function() {
+        //     window.open("/addcategory", "_self");
+        //     alert("Success");
+        // });
+        //$("bodycontent").load("/addcategory");
+    });
+});
