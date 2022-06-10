a24fd7f08b95c43d670ba85ecd009696ed9ade9e
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-06-10 14:38:00 +0700

    Revert "Revert "add folder octopus/dashboard""
    This reverts commit 8b87225b377a0ebed8c05cb8f8e029cb4092b480.

--- resources/views/admin/layout/adminlayout.blade.php
+++ resources/views/admin/layout/adminlayout.blade.php
@@ -466,6 +466,7 @@
 <!-- Theme Initialization Files -->
 <script src={{asset('octopus/octopus/assets/javascripts/theme.init.js')}}></script>
 
-
+<!-- Example -->
+<script src={{asset('octopus/octopus/assets/javascripts/dashboard/examples.dashboard.js')}}></script>
 </body>
 </html>
